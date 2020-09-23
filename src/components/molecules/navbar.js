3392030a9b294f6eeaf64ad9a5a631139e17a7e8
Duvan Moreno <duvanmorenocardona.dmc@gmail.com>
2020-09-23 13:36:53 -0500

    Style navbar, Card , Button and Sections
    * Add estructure to Component
    * Create variables
    * Style card

--- src/components/molecules/navbar.js
+++ src/components/molecules/navbar.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function NavBar () {
     return (
-        <div>
-            NavBar
-        </div>
+        <section className="section-navbar">
+            <h2>
+                watch
+                <span className="gradient-text">
+                    MOVIE
+                </span>
+            </h2>
+        </section>
     )
 }
--- src/components/views/DetailMovie.js
+++ src/components/views/DetailMovie.js
@@ -1,11 +1,98 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function DetailMovie() {
+/* Import Externals Components */
+import { useParams } from 'react-router-dom';
+
+/* Import Redux */
+import { connect } from 'react-redux'
+import { getDataMovie, postAxios, getAuthGuestSession } from '../../redux/actions/moviesActions'
+import StarRating from '../molecules/StarRating';
+
+
+function DetailMovie ( { getDataMovie, postAxios, getAuthGuestSession, title, overview, genres, runtime, vote_average, release_date, poster_path, backdrop_path, state, guest_session_id } ) {
+
+    const { id } = useParams()
+
+    useEffect( () => {
+        getAuthGuestSession()
+        getDataMovie( id )
+    }, [id] )
+
+    useEffect( () => {
+        console.log( "DetailMovie -> state", state )
+    }, [state] )
+
     return (
-        <div>
-            Movie with 
+        <div className="detail-movie-section" >
+            <div className="backdrop-movie" style={{
+                "background-image": `url(https://image.tmdb.org/t/p/original${backdrop_path})`
+            }}>
+                <section className="section-detail-movie" >
+                    <figure>
+                        <img
+                            src={`https://image.tmdb.org/t/p/original${poster_path}`}
+                            style={{ width: "100%" }}
+                        />
+                    </figure>
+                    <div>
+                        <h3>
+                            {title}
+                        </h3>
+                        {release_date}
+                        <p>
+                            <span>
+                                {
+                                    genres.length > 0 &&
+                                    genres.map( ( item ) => (
+                                        `${item.name},`
+                                    ) )
+                                }
+                            </span>
+                        -
+                        <span>
+                                {runtime}
+                            </span>
+                        </p>
+                        <p>
+                            {vote_average}
+                        </p>
+                        <article>
+                            <h5>
+                                Overview
+                        </h5>
+                            <p>
+                                {overview}
+                            </p>
+                        </article>
+                    </div>
+                </section>
+            </div>
+            <button onClick={() => postAxios( id, 2, guest_session_id )} >
+                Post Active
+            </button>
+            <div>
+                <h4>
+                    Rating Movie
+                </h4>
+                <StarRating/>
+            </div>
         </div>
     )
 }
 
-export default DetailMovie
+DetailMovie.defaultProps = {
+    genres: []
+}
+
+const mapStateToProps = ( { detailMovieReducer } ) => {
+    return detailMovieReducer
+}
+
+const mapDispatchToProps = {
+    postAxios,
+    getDataMovie,
+    getAuthGuestSession
+}
+
+
+export default connect( mapStateToProps, mapDispatchToProps )( DetailMovie )
